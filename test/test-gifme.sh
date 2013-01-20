3d73bd64155c60ac4aed1e8dd93b6ec7e36de715
Zach Holman <zach@zachholman.com>
2013-01-20 12:54:07 -0800

    Start adding some shit

--- test/test-gifme.sh
+++ test/test-gifme.sh
@@ -4,6 +4,14 @@ describe "gifme: animated gifs"
 
 gifme="./gifme"
 
+it_needs_imagemagick() {
+  path=$PATH
+  PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+  output=$($gifme || true)
+  echo $output | grep 'imagemagick not found'
+}
+
 it_shows_help_with_no_argv() {
   $gifme | grep USAGE
 }
